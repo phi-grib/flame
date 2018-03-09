bfe616e77b876e0ab8edf90a432d5a6928d1ae71
josecarlosgomezt <jcgomez@akasha.prib.upf.edu>
2018-03-09 12:17:25 +0100

    -base_model cleaned- -qualitative prediction added. DIPL2 endpoint

---
+++ src/DIPL2/control_child.py
@@ -0,0 +1,36 @@
+#! -*- coding: utf-8 -*-
+
+##    Description    Flame Control internal class
+##
+##    Authors:       Manuel Pastor (manuel.pastor@upf.edu)
+##
+##    Copyright 2018 Manuel Pastor
+##
+##    This file is part of Flame
+##
+##    Flame is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 3.
+##
+##    Flame is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with Flame. If not, see <http://www.gnu.org/licenses/>.
+
+from control import Control
+
+class ControlChild (Control):
+
+    def __init__ (self):
+
+        Control.__init__ (self)
+
+        self.model_name = 'DIPL2'
+        self.model_version = 0.1
+        self.quantitative = False
+        self.tune = False
+
+        return
--- src/base_model.py
+++ src/base_model.py
@@ -1,3 +1,24 @@
+#! -*- coding: utf-8 -*-
+
+##    Description    Flame Parent Model Class
+##
+##    Authors:       Jose Carlos Gómez (josecarlos.gomez@upf.edu)
+##
+##    Copyright 2018 Manuel Pastor
+##
+##    This file is part of Flame
+##
+##    Flame is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 3.
+##
+##    Flame is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with Flame.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import numpy as np
@@ -19,8 +40,8 @@ from sklearn.model_selection import GridSearchCV  # JC
 from sklearn.metrics import mean_squared_error, matthews_corrcoef as mcc
 from sklearn.metrics import f1_score
 from sklearn.metrics import make_scorer
+from sklearn.metrics import confusion_matrix
 
-#from sklearn.model_selection import *  # JC
 from model_validation import *
 
 from scale import center, scale
@@ -54,11 +75,13 @@ class BaseEstimator(object):
         self.TN = 0
         self.FP = 0
         self.FN = 0
+        self.sensitivity = 0.00
+        self.specificity = 0.00
         self.mcc = 0
         self.SDEP = 0.00
         self.Q2 = 0.00
         self.scoringP = 0.00
-        self.OOBe = 0.00
+
         
         # Goodness of the fit restults
         
@@ -66,6 +89,8 @@ class BaseEstimator(object):
         self.TNpred = 0
         self.FPpred = 0
         self.FNpred = 0
+        self.sensitivityPred = 0.00
+        self.specificityPred = 0.00
         self.SDEC = 0.00    # SD error of the calculations
         self.R2 = 0.00    # determination coefficient
         self.scoringR = 0.00
@@ -78,14 +103,29 @@ class BaseEstimator(object):
         self.conformal_pred = None
         self.meanConformalInterval = 0.00
 
-    def validate(self):
-        """ Validates the models and completes suitable scoring values
+    
+    def printQuantitativeValidationResults(self):
 
+        print ("Recalculated results")
+        print ('rec R2:%5.3f SDEC:%5.3f  mean_squared_error:%5.3f' %
+                (self.R2,self.SDEC, self.scoringR))
+        print (str(self.cv)+" cross-validation results")
+        print ('pred R2:%5.3f Q2:%5.3f SDEP:%5.3f mean_squared_error:%5.3f' % \
+                (self.R2,self.Q2,self.SDEP, self.scoringP))
+    
+    def printQualitativeValidationResults(self):
+        print ("Recalculated results")
+        print ("rec  TP:%d TN:%d FP:%d FN:%d spec:%5.3f sens:%5.3f MCC:%5.3f" % \
+                (self.TPpred, self.TNpred, self.FPpred, self.FNpred, self.specificityPred,
+                 self.sensitivityPred, self.mccp))
 
-        """
+        print (str(self.cv)+" cross-validation results")
+        print ("pred  TP:%d TN:%d FP:%d FN:%d spec:%5.3f sens:%5.3f MCC:%5.3f" % \
+                (self.TP, self.TN, self.FP, self.FN, self.specificity, self.sensitivity, self.mcc))
 
-        if ("PLSR" in self.name): pass ## Override for PLS_DA
-         
+    
+    def quantitativeValidation(self):
+        
         nobj, nvarx = np.shape(self.X)
         if self.X is None or self.estimator is None:
             print ("no estimator")
@@ -95,214 +135,99 @@ class BaseEstimator(object):
         Y = self.Y.copy()
 
         if self.autoscale:
-            X = X-self.mux
-            X = X*self.wgx
+            X = X - self.mux
+            X = X * self.wgx
 
         Yp = self.estimator.predict(X)
         Ym = np.mean(Y)
 
-        # Quantitative
+        SSY0 = np.sum (np.square(Ym-Y))
+        SSY  = np.sum (np.square(Yp-Y))
+
+        self.scoringR = np.mean(mean_squared_error(Y, Yp)) # Mean Squared Error
+        self.SDEC = np.sqrt(SSY/nobj)
+        self.R2   = 1.00 - (SSY/SSY0)
+
+        ## Cross-validation
+        y_pred = cross_val_predict(copy.copy(self.estimator), copy.copy(X), 
+                                             copy.copy(Y), cv=self.cv, n_jobs=-1)
+
+        # Mean Squared Error
+        SSY0_out = np.sum(np.square(Ym - Y))
+        SSY_out = np.sum(np.square(Y - y_pred))
+        self.scoringP = mean_squared_error(Y, y_pred)
+        self.SDEP = np.sqrt(SSY_out/(nobj))
+        self.Q2   = 1.00 - (SSY_out/SSY0_out)
+
+
+    def qualitativeValidation(self):
+        
+        nobj, nvarx = np.shape(self.X)
+        if self.X is None or self.estimator is None:
+            print ("no estimator")
+            return
+
+        X = self.X.copy()
+        Y = self.Y.copy()
+
+        if self.autoscale:
+            X = X - self.mux
+            X = X * self.wgx
+
+        Yp = self.estimator.predict(X)
+
+        if len(Yp) != len(Y):
+            return
+        
+        print ('Goodness of the fit validation')
+        self.TPpred, self.FPpred, self.FNpred, self.TNpred = confusion_matrix(Y,Yp).ravel()
+        self.sensitivityPred = (self.TPpred / (self.TPpred + self.FNpred))
+        self.specificityPred = (self.TNpred / (self.TNpred + self.FPpred))
+        self.mccp  = mcc(Y, Yp)
+
+
+        # Cross validation
+        print ('Cross validating')
+        y_pred = cross_val_predict(self.estimator, X, Y, cv=self.cv, n_jobs=-1)
+        self.TP, self.FP, self.FN, self.TN = confusion_matrix(Y,y_pred).ravel()
+        self.sensitivity = (self.TP / (self.TP + self.FN))
+        self.specificity = (self.TN / (self.TN + self.FP))
+        self.mcc  = mcc(Y, y_pred)
+
+
+    """ Validates the models and completes suitable scoring values"""
+
+    def validate(self):
+        
         if self.quantitative:
-            SSY0 = np.sum (np.square(Ym-Y))
-            SSY  = np.sum (np.square(Yp-Y))
-            NMSErec = np.mean(mean_squared_error(Y, Yp)) # Mean Squared Error
-            self.scoringR = NMSErec
-            self.SDEC = np.sqrt(SSY/nobj)
-            self.R2   = 1.00 - (SSY/SSY0)
-
-            if "RF" in self.name:
-                self.OOBe = 1.00 - self.estimator.oob_score_
-                print ("Recalculated results")
-                print ('rec R2:%5.3f SDEC:%5.3f OOB_error:%5.3f neg_mean_squared_error:%5.3f' %
-                      (self.R2,self.SDEC,self.OOBe, self.scoringR))
-
-            else:
-                print ("Recalculated results")
-                print ('rec R2:%5.3f SDEC:%5.3f  neg_mean_squared_error:%5.3f' %
-                      (self.R2,self.SDEC, self.scoringR))
-
-
-
-            scoring = 'neg_mean_squared_error'
-
-            y_pred = cross_val_predict(copy.copy(self.estimator), copy.copy(X), copy.copy(Y), cv=self.cv, n_jobs=-1)
-
-            NMSEcv = np.mean(cross_val_score(self.estimator, X, Y, cv=self.cv, scoring=scoring, n_jobs=-1)) # Mean Squared Error
-            SSY0_out = np.sum(np.square(Ym - Y))
-            SSY_out = np.sum(np.square(Y - y_pred))
-            self.scoringP = NMSEcv
-            self.SDEP = np.sqrt(SSY_out/(nobj))
-            self.Q2   = 1.00 - (SSY_out/SSY0_out)
-            # OOBe_loo  = 1.00 - np.mean(OOB_errors)
-
-            print (str(self.cv)+" cross-validation results")
-            print ('pred R2:%5.3f Q2:%5.3f SDEP:%5.3f neg_mean_squared_error:%5.3f' % \
-                  (self.R2,self.Q2,self.SDEP, self.scoringP))
-
-            # GRAPHS
-
-            # pngfiles = glob.glob (self.vpath+'/*.png')
-            # for i in pngfiles:
-            #     os.remove(i)
-            # try:
-            #     fig1=plt.figure()
-            #     plt.xlabel('experimental y')
-            #     plt.ylabel('recalculated\n',fontsize=14)
-            #     plt.title('R2: %4.2f  /  SDEC: %4.2f \n' % (self.R2,self.SDEC), fontsize=14)
-            #     plt.plot(Y,Yp,"ro")
-            #     fig1.savefig(self.vpath+"/" + self.name + "-recalculated.png", format='png')
-            #     fig1.savefig("./" + self.name + "-recalculated.png", format='png')
-            # except:
-            #     print "Error creating Recalculated vs Experimental" + self.name + " model graph"
-
-            # try:
-            #     fig1=plt.figure()
-            #     plt.xlabel('experimental y')
-
-            #     plt.ylabel('predicted\n',fontsize=14)
-            #     plt.title('Q2: %4.2f  /  SDEP: %4.2f \n' % (self.Q2,self.SDEP), fontsize=14)
-            #     plt.plot(Y, y_pred,"ro")
-            #     fig1.savefig(self.vpath+"/" + self.name + "-predicted.png", format='png')
-            #     fig1.savefig("./" + self.name + "-predicted.png", format='png')
-            # except:
-            #     print "Error creating Predicted vs Experimental " + self.name + " model graph"
-
-            #if self.conformal:
-            #    self.meanConformalInterval = CF_QuanVal(X, Y, copy.copy(self.estimator), self.conformalSignificance)
-
-
-        ######################################################################3
-        ### qualitative
+            self.quantitativeValidation()
+            self.printQuantitativeValidationResults()
         else:
-
-
-            # I think this is not needed.... by the characteristics of RF it allways shows perfect performance
-            if len(Yp) != len(Y):
-                return
-
-            TP=TN=FP=FN=0
-
-            for i in range(len(Y)):
-
-                if Y[i] == 1.0:
-                    if Yp[i] == 1.0:
-                        TP+=1
-                    else:
-                        FN+=1
-                else:
-                    if Yp[i] == 1.0:
-                        FP+=1
-                    else:
-                        TN+=1
-
-            if TP+TN+FP+FN == 0:
-                print ('no objects')
-                return
-
-            self.TPpred = TP
-            self.TNpred = TN
-            self.FPpred = FP
-            self.FNpred = FN
-
-            sens = sensitivity (TP, FN)
-            spec = specificity (TN, FP)
-            self.mccp  = MCC (TP, TN, FP, FN)
-            print ("MCCaa")
-            f1   = f1_score(Y, Yp, pos_label=1, average='binary')
-            if "RF" in self.name:
-                self.OOBe = 1.00 - self.estimator.oob_score_
-                print ("Recalculated results")
-                print ("rec  TP:%d TN:%d FP:%d FN:%d spec:%5.3f sens:%5.3f MCC:%5.3f OOB_error:%5.3f f1_score:%5.3f" % \
-                      (TP, TN, FP, FN, spec, sens, self.mcc, self.OOBe, f1 ))
-            else:
-                print ("Recalculated results")
-                print ("rec  TP:%d TN:%d FP:%d FN:%d spec:%5.3f sens:%5.3f MCC:%5.3f f1_score:%5.3f" % \
-                      (TP, TN, FP, FN, spec, sens, self.mcc, f1 ))
-
-            # Leave-one-out Cross validation
-            print ('Cross validating')
-            scoring = 'f1'
-
-            y_pred = cross_val_predict(self.estimator, X, Y, cv=self.cv, n_jobs=-1)
-
-
-            TPo=TNo=FPo=FNo = 0
-
-            for i in range(len(Y)):
-
-                if Y[i] == 1.0:
-                    if y_pred[i] == 1.0:
-                        TPo+=1
-                    else:
-                        FNo+=1
-                else:
-                    if y_pred[i] == 1.0:
-                        FPo+=1
-                    else:
-                        TNo+=1
-
-            if TPo+TNo+FPo+FNo == 0:
-                return
-
-            self.TP = TPo
-            self.TN = TNo
-            self.FP = FPo
-            self.FN = FNo
-
-            sens_cv = sensitivity (TPo, FNo)
-            spec_cv = specificity (TNo, FPo)
-            self.mcc  = MCC (TPo, TNo, FPo, FNo)
-            f1_cv = f1_score(Y, y_pred, pos_label=1, average='binary')
-
-
-
-
-            print (str(self.cv)+" cross-validation results")
-            print ("pred  TP:%d TN:%d FP:%d FN:%d spec:%5.3f sens:%5.3f MCC:%5.3f f1_score:%5.3f" % \
-                  (TPo, TNo, FPo, FNo, spec_cv, sens_cv, self.mccp, f1_cv ))
-
-            # Create Graphs
-
-            # pngfiles = glob.glob (self.vpath+'/*.png')
-            # for i in pngfiles:
-            #     os.remove(i)
-
-            # # Predicted confusion matrix graph
-            # try:
-            #     FourfoldDisplay(TPo,TNo,FPo,FNo, self.name +'-Predicted', self.name +'_predicted_confusion_matrix.png' , self.vpath)
-            # except:
-            #     print "Failed to generate RF predicted validation graph"
-
-            # # Recalculated confusion matrix graph
-            # try:
-            #     FourfoldDisplay(TP,TN,FP,FN, self.name + ' Recalculated',  self.name +'_recalculated_confusion_matrix.png' , self.vpath)
-            # except:
-            #     print "Failed to generate RF recalculated validation graph"
-
-        #if self.conformal:
-        #    self.conformal_pred = CF_QualVal(X, Y, copy.copy(self.estimator), self.conformalSignificance)
-
+            self.qualitativeValidation()
+            self.printQualitativeValidationResults()
+            return
 
         # Move this to an external module ****
-        if self.learning_curve:
-            print ('Building Learning Curves')
-            title = "Learning Curves (RF)"
-            cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-            estimator = self.clf
-            plot = plot_learning_curve(estimator, title, self.X, self.Y, (0.0, 1.01), cv=cv)
-            plot.savefig(self.vpath+"/" + self.name + "-learning_curves.png", format='png')
-            plot.savefig("./" + self.name + "-learning_curves.png", format='png')
+        
+        # if self.learning_curve:
+        #     print ('Building Learning Curves')
+        #     title = "Learning Curves (RF)"
+        #     cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+        #     estimator = self.clf
+        #     plot = plot_learning_curve(estimator, title, self.X, self.Y, (0.0, 1.01), cv=cv)
+        #     plot.savefig(self.vpath+"/" + self.name + "-learning_curves.png", format='png')
+        #     plot.savefig("./" + self.name + "-learning_curves.png", format='png')
 
-        return (Yp)
+        # return (Yp)
 
 
     def project (self, Xb):
         """ Uses the X matrix provided as argument to predict Y
         """
 
-#        if self.autoscale:
-#            Xb = Xb-self.mux
-#            Xb = Xb*self.wgx
+       if self.autoscale:
+           Xb = Xb-self.mux
+           Xb = Xb*self.wgx
 
          # Xb = Xb.reshape(1,-1) # required by sklean, to avoid deprecation warning
         Yp = self.estimator.predict(Xb)
