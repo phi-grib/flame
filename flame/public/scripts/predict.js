787af1c707e4d379ec535c28257677e0fe39b22a
Manuel Pastor <mpastor@localhost.localdomain>
2018-04-03 19:48:42 +0200

    GNU headers for JS code

--- flame/public/scripts/predict.js
+++ flame/public/scripts/predict.js
@@ -1,3 +1,23 @@
+//     Description    Flame Predict JavaScript 
+//
+//     Authors:       Manuel Pastor (manuel.pastor@upf.edu)
+// 
+//     Copyright 2018 Manuel Pastor
+// 
+//     This file is part of Flame
+// 
+//     Flame is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation version 3.
+// 
+//     Flame is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with Flame. If not, see <http://www.gnu.org/licenses/>.
+
 // parse results obtained from the prediction
 function parseResults (results) {
     $("#data-body").text(results);
