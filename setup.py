376d00b12e85d3d14f21c9fee43ff4e709111fb4
bet-gregori <elisabet.gregori@gmail.com>
2018-04-05 13:21:49 +0200

    Added setup scripts

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='flame',
+      version='0.1',
+      licence='GNU GPLv3',
+      description='',
+      url='https://github.com/phi-grib/flame',
+      download_url='https://github.com/phi-grib/flame.git',
+      author='Manuel Pastor, Biel Stela, Jose Carlos Gomez',
+      author_email='manuel.pastor@upf.edu',
+      packages=['flame']
+    )
