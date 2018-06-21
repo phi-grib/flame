20ac1e27bc8e5ecd32408bae9ac911c6e335714a
BielStela <biel.stela@gmail.com>
2018-06-21 12:24:38 +0200

    updates and changed flame main name to avoid script conflicts

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(name='flame',
       version='0.1',
-      licence='GNU GPLv3 or posterior',
+      license='GNU GPLv3 or posterior',
       description='',
       url='https://github.com/phi-grib/flame',
       download_url='https://github.com/phi-grib/flame.git',
       author='Manuel Pastor, Biel Stela, Jose Carlos Gomez',
       author_email='manuel.pastor@upf.edu',
       packages=find_packages(),
-      entry_points = {
-        'console_scripts': ['flame=flame.flame:main'],
+      entry_points={
+        'console_scripts': ['flame=flame.flame_scr:main'],
         }
       )
