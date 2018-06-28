4d4ceb27bf7ecfef3bec1b02a80a0bbf2a067f58
BielStela <biel.stela@gmail.com>
2018-06-28 11:01:42 +0200

    added new model test

--- flame/tests/test_manage.py
+++ flame/tests/test_manage.py
@@ -1,4 +1,13 @@
 
 import pytest
-
+import pathlib
 from flame import manage
+
+
+def test_manage_new_model():
+    manage.set_model_repository('/home')
+    manage.action_new('TESTMODEL')
+    home_dirs = list(pathlib.Path('/home').iterdir())
+
+    case = pathlib.Path('/home/TESTMODEL')
+    assert case in home_dirs
