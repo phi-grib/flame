6d5123b48f53a85dbcc3b0404387ac759432d4ca
BielStela <biel.stela@gmail.com>
2018-06-29 09:37:25 +0200

    bfscript new dir

--- flame/tests/test_manage.py
+++ flame/tests/test_manage.py
@@ -5,9 +5,9 @@ from flame import manage
 
 
 def test_manage_new_model():
-    manage.set_model_repository('/home')
+    manage.set_model_repository('/home/testmodels')
     manage.action_new('TESTMODEL')
-    home_dirs = list(pathlib.Path('/home').iterdir())
+    home_dirs = list(pathlib.Path('/home/testmodels').iterdir())
 
     case = pathlib.Path('/home/TESTMODEL')
     assert case in home_dirs
