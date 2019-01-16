374373d289cb8f203d0cd381e07790a78d3f37aa
Manuel Pastor <manuel.pastor@upf.edu>
2019-01-16 09:30:14 +0100

    new parameters constructor

--- flame/parameters.py
+++ flame/parameters.py
@@ -21,74 +21,92 @@
 # along with Flame. If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import sys
 import yaml
 
-from flame.util import utils, get_logger
-
-LOG = get_logger(__name__)
+from flame.util import utils
 
 class Parameters:
+    ''' Class storing a large set of parameters defining how a model is built
 
-        def __init__(self, model, version):
-     
-            parameters_file_path = utils.model_path(model, version)
-            parameters_file_name = os.path.join (parameters_file_path,'parameters.yaml')
+        These parameters are loaded from a configuration file (typically 
+        in yaml format) 
 
-            if not os.path.isfile(parameters_file_name):
-                LOG.critical('Unable to load model parameters. Aborting...')
-                sys.exit()
+        Every parameter is a dictionary with keys defining the parameter type, 
+        value and providing a human-readable explanation used for the GUI
+    '''
 
-            try:
-                with open(parameters_file_name, 'r') as pfile:
-                    self.p = yaml.load(pfile)
-            except Exception as e:
-                LOG.critical('Unable to load model parameters. Aborting...')
-                sys.exit()
+    def __init__(self):
+        ''' constructor '''
+        return
 
-            self.setVal('endpoint',model)
-            self.setVal('version',version)
-            self.setVal('model_path',parameters_file_path)
-            self.setVal('md5',utils.md5sum(parameters_file_name))
+    def loadDict (self, d):
+        ''' load the content from a dictionary '''
+        self.p = d    
+        return
 
-            return
+    def loadYaml (self, model, version):       
+        ''' load a set of parameters from the configuration file present 
+            at the model directory
 
-        def getVal(self, key):
-            ''' Return the value of the key parameter or None if it is
-                not found in the parameters dictionary
-            ''' 
-            if key in self.p:
-                return self.p[key]
-            return None
+            adds some parameters identifying the model and the 
+            hash of the configuration file 
+        '''
+        parameters_file_path = utils.model_path(model, version)
+        parameters_file_name = os.path.join (parameters_file_path,
+                                            'parameters.yaml')
 
-        def getOldParam(self):
-            ''' Returns the dictionary with the parameters
-                This function was defined only for compatibility purposes
-                during the implementation of this class
-            '''
-            return self.p
+        if not os.path.isfile(parameters_file_name):
+            return False
 
-        def setVal(self, key, value):
-            ''' Sets the parameter defined by key to the given value
-            '''
-            self.p[key] = value
+        try:
+            with open(parameters_file_name, 'r') as pfile:
+                self.p = yaml.load(pfile)
+        except Exception as e:
+            return False
 
-        def appVal(self, key, value):
-            ''' Appends value to the end of existing key list 
-            '''
-            self.p[key].append(value)
+        self.setVal('endpoint',model)
+        self.setVal('version',version)
+        self.setVal('model_path',parameters_file_path)
+        self.setVal('md5',utils.md5sum(parameters_file_name))
 
-        def getModelSet (self):
-            ''' Returns a Boolean indicating if the model uses external input
-                sources and a list with these sources 
-            '''
-            ext_input = False
-            model_set = None
+        return True
 
-            if self.getVal('ext_input'):
-                model_set = self.getVal('model_set')
-                if model_set is not None:
-                    if len(model_set) > 1:
-                        ext_input = True
+    def getVal(self, key):
+        ''' Return the value of the key parameter or None if it is
+            not found in the parameters dictionary
+        ''' 
+        if key in self.p:
+            return self.p[key]
+        return None
 
-            return ext_input, model_set
+    def getOldParam(self):
+        ''' Returns the dictionary with the parameters
+            This function was defined only for compatibility purposes
+            during the implementation of this class
+        '''
+        return self.p
+
+    def setVal(self, key, value):
+        ''' Sets the parameter defined by key to the given value
+        '''
+        self.p[key] = value
+
+    def appVal(self, key, value):
+        ''' Appends value to the end of existing key list 
+        '''
+        self.p[key].append(value)
+
+    def getModelSet (self):
+        ''' Returns a Boolean indicating if the model uses external input
+            sources and a list with these sources 
+        '''
+        ext_input = False
+        model_set = None
+
+        if self.getVal('ext_input'):
+            model_set = self.getVal('model_set')
+            if model_set is not None:
+                if len(model_set) > 1:
+                    ext_input = True
+
+        return ext_input, model_set
