099f968918db62001a0420c37165f77fa8fa73e4
josecarlosgomezt <josecarlos.gomez@upf.edu>
2019-01-29 18:06:03 +0100

    Loading of files implemented in load method

--- flame/documentation.py
+++ flame/documentation.py
@@ -23,6 +23,7 @@
 import os
 import yaml
 import json
+import pickle
 
 from flame.util import utils
 from flame.util import get_logger
@@ -34,7 +35,7 @@ class Documentation:
         Fields are loaded from a YAML file (documentation.yaml)
 
         ...
-        
+
         Attributes
         ----------
 
@@ -42,11 +43,11 @@ class Documentation:
             fields in the documentation
         version : int
             documentation version
-        
-        
+
+
         Methods
         -------
-        
+
         load_results(results_dictionary)
             Accesses to build results and param class to retrieve all
             information needed to document the model.
@@ -60,10 +61,13 @@ class Documentation:
     def __init__(self, model, version=0):
         ''' Load the fields from the documentation file'''
 
+        self.model = model
+        self.version = version
+
         # obtain the path and the default name of the model documents
-        documentation_file_path = utils.model_path(model, version=0)
+        documentation_file_path = utils.model_path(self.model, self.version)
         documentation_file_name = os.path.join(documentation_file_path,
-                                              'documentation.yaml')
+                                               'documentation.yaml')
 
         # load the main class dictionary (p) from this yaml file
         if not os.path.isfile(documentation_file_name):
@@ -80,7 +84,37 @@ class Documentation:
         # self.fields['md5'] = utils.md5sum(documentation_file_name)
         #self.fields['md5']
 
-    def load_results(self, results, params):
+    def load_results(self):
         '''This function takes info from results and
         param file and assigns it to corresponding fields
         in documentation dictionary'''
+
+        # obtain the path and the default name of the parameter_file
+        parameters_file_path = utils.model_path(self.model, self.version)
+        parameters_file_name = os.path.join(parameters_file_path,
+                                            'parameters.yaml')
+
+        if not os.path.isfile(parameters_file_name):
+            raise Exception('Parameter file not found')
+        try:
+            with open(parameters_file_name, 'r') as parameter_file:
+                self.fields = yaml.load(parameter_file)
+        except Exception as e:
+            LOG.error(f'Error loading parameter file with exception: {e}')
+            raise e
+
+        # obtain the path and the default name of the results file
+        results_file_path = utils.model_path(self.model, self.version)
+        results_file_name = os.path.join(results_file_path,
+                                         'parameters.yaml')
+
+        # load the main class dictionary (p) from this yaml file
+        if not os.path.isfile(results_file_name):
+            raise Exception('Results file not found')
+
+        try:
+            with open(results_file_name, "rb") as input_file:
+                results = pickle.load(input_file)
+        except Exception as e:
+            LOG.error(f'No valid results pickle found at: {results_file_name}')
+            raise e
