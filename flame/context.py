0727473d3dd94f76610ba0ed25419eed2f801ea1
josecarlosgomezt <josecarlosgomezt@gmail.com>
2020-03-23 12:46:09 +0100

    Imbalance performed with numpy and fixed bug at checking incremental argument

--- flame/context.py
+++ flame/context.py
@@ -202,8 +202,8 @@ def build_cmd(arguments, output_format=None):
         if ifile is not None:
             if not os.path.isfile(ifile):
                 return False, f'Wrong training series file {ifile}'
-        
-            if 'incremental' in arguments:    
+
+            if  arguments['incremental']:                
                 if arguments['incremental'] and os.path.isfile(lfile):
                     LOG.info(f'Merging file {ifile} with existing training series')
                     new_training = os.path.join(epd, 'temp_training')
