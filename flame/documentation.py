53c3efa5e2fce021cf87e2a4d58a81e4fd2db085
Manuel Pastor <manuel.pastor@upf.edu>
2021-01-16 12:01:19 +0100

    new documentation fields

--- flame/documentation.py
+++ flame/documentation.py
@@ -309,14 +309,16 @@ class Documentation:
     def dumpYAML (self):
         yaml_out = []
 
-        order = ['ID', 'Version', 'Contact', 'Institution', 'Date', 'Endpoint',
-            'Endpoint_units', 'Interpretation', 'Dependent_variable', 'Species',
+        order = ['ID', 'Version', 'Model_title', 'Model_description', 'Keywords', 
+        'Contact', 'Institution', 'Date', 'Endpoint',
+        'Endpoint_units', 'Interpretation', 'Dependent_variable', 'Species',
         'Limits_applicability', 'Experimental_protocol', 'Model_availability',
         'Data_info', 'Algorithm', 'Software', 'Descriptors', 'Algorithm_settings',
         'AD_method', 'AD_parameters', 'Goodness_of_fit_statistics', 
         'Internal_validation_1', 'Internal_validation_2', 'External_validation',
         'Comments', 'Other_related_models', 'Date_of_QMRF', 'Date_of_QMRF_updates',
-        'QMRF_updates', 'References', 'QMRF_same_models', 'Comment_on_the_endpoint',
+        'QMRF_updates', 'References', 'QMRF_same_models', 'Mechanistic_basis', 
+        'Mechanistic_references', 'Supporting_information', 'Comment_on_the_endpoint',
         'Endpoint_data_quality_and_variability', 'Descriptor_selection'
         ]
 
