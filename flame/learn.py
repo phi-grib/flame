46bf4f330560fdd3af0ecec81438606b894a945a
Pablo Rodríguez Belenguer <62376157+PARODBE@users.noreply.github.com>
2021-03-23 18:15:20 +0100

    mani

--- flame/learn.py
+++ flame/learn.py
@@ -39,8 +39,8 @@ from flame.stats.combo import median, mean, majority, logicalOR, matrix
 from flame.stats.imbalance import run_imbalance  
 
 # from flame.graph.graph import generateProjectedSpace
-# from flame.graph.graph import generateManifoldSpace
-from flame.graph.graph import generatetsneSpace
+from flame.graph.graph import generateManifoldSpace
+# from flame.graph.graph import generatetsneSpace
 
 from flame.util import utils, get_logger
 LOG = get_logger(__name__)
@@ -392,8 +392,8 @@ class Learn:
 
         # generate a proyected space and use it to generate graphics
         # generateProjectedSpace(self.X, self.param, self.conveyor)
-        # generateManifoldSpace(self.X, self.param, self.conveyor)
-        generatetsneSpace (self.X,self.Y,self.param,self.conveyor)
+        generateManifoldSpace(self.X, self.param, self.conveyor)
+        # generatetsneSpace (self.X,self.Y,self.param,self.conveyor)
 
 
         LOG.info('Model finished successfully')
