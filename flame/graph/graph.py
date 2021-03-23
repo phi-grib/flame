bf9ef1b4775cab0541d6e71a9558141ba85e1c92
Pablo Rodríguez Belenguer <62376157+PARODBE@users.noreply.github.com>
2021-03-23 18:16:28 +0100

    mani

--- flame/graph/graph.py
+++ flame/graph/graph.py
@@ -45,7 +45,7 @@ def generateManifoldSpace(X,param,conveyor):
 
     # a = time.time()
 
-    pca = PCA(n_components=100,random_state=46)
+    pca = PCA(n_components=15,random_state=46)
     X_pca = pca.fit_transform(X)
 
     # print ('PCA generated in: ', a-time.time())
