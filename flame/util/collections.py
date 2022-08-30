c2ebec57568090034738adc6a44fed248f6e0802
AdrianCabreraPhi <adridominican@gmail.com>
2022-08-30 12:27:09 +0200

    implemented get collections

--- flame/util/collections.py
+++ flame/util/collections.py
@@ -4,8 +4,7 @@ from flame.util import utils,get_logger
 LOG = get_logger(__name__)
 
 def createCollection(nameCollection,endpoints,versions):
-
-    collections_list = {}
+    collections_list = []
     meta_path = os.path.dirname(os.path.dirname(__file__))
     collections_file = os.path.join(meta_path, 'collections.pkl')
     # check if exist collection
@@ -14,9 +13,27 @@ def createCollection(nameCollection,endpoints,versions):
             collections_list = pickle.load(file)
 
     # add new collection 
-    collections_list[nameCollection] = {'endpoints':endpoints,'versions':versions}
+    collections_list.append({'name':nameCollection,'endpoints':endpoints,'versions':versions})
+
+    print(collections_list)
     with open(collections_file,"wb") as file:
         pickle.dump(collections_list,file)
         LOG.info(f'Save collections.pkl file \n')
 
-    return "Save collection successfully"    
+    return "Save collection successfully"
+
+
+def get_collections():
+    '''
+    Retrieves the collection list
+    '''
+    meta_path = os.path.dirname(os.path.dirname(__file__))
+    collections_file = os.path.join(meta_path, 'collections.pkl')
+    if os.path.isfile(collections_file):
+        with open(collections_file,"rb") as file:
+            collections_list = pickle.load(file)
+            file.close()
+            return collections_list
+    
+    return False
+
