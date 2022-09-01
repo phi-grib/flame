f5525c1cc1c2c543975853deac2f56c7776c240f
AdrianCabreraPhi <adridominican@gmail.com>
2022-09-01 10:46:37 +0200

    collections

--- flame/util/collections.py
+++ flame/util/collections.py
@@ -1,20 +1,21 @@
 import os
 import pickle
-from flame.util import utils,get_logger 
+from flame.util import utils,get_logger
+from datetime import datetime 
 LOG = get_logger(__name__)
 
 def createCollection(nameCollection,endpoints,versions):
     collections_list = []
+    date = datetime.now().strftime('%Y-%m-%d')
     meta_path = os.path.dirname(os.path.dirname(__file__))
     collections_file = os.path.join(meta_path, 'collections.pkl')
     # check if exist collection
     if os.path.isfile(collections_file):
         with open(collections_file,"rb") as file:
             collections_list = pickle.load(file)
-
-    # add new collection 
-    collections_list.append({'name':nameCollection,'endpoints':endpoints,'versions':versions})
-
+            
+    # add new collection
+    collections_list.append({'name':nameCollection,'endpoints':endpoints,'versions':versions,'date':date})
     print(collections_list)
     with open(collections_file,"wb") as file:
         pickle.dump(collections_list,file)
@@ -36,4 +37,3 @@ def get_collections():
             return collections_list
     
     return False
-
