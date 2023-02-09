a89965ec4711a084bb8e76ee17f38545030522ad
Manuel Pastor <manuel.pastor@upf.edu>
2023-02-09 18:42:55 +0100

    version change

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flame',
-    version='1.0.1',
+    version='1.2.0',
     license='GNU GPLv3 or posterior',
     description='',
     url='https://github.com/phi-grib/flame',
